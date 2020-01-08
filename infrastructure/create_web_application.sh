1827f0a646c71fcc17fe6f29578fe827ca0b53e8
Ian Cornett <ian.cornett@slalom.com>
2020-01-08 13:49:29 -0800

    fix missing param

--- infrastructure/create_web_application.sh
+++ infrastructure/create_web_application.sh
@@ -4,4 +4,4 @@ GROUP=$2
 
 az appservice plan create --resource-group $GROUP --name $NAME --sku FREE
 
-az webapp create --name $NAME --resource-group $GROUP
+az webapp create --name $NAME --resource-group $GROUP --plan $NAME
