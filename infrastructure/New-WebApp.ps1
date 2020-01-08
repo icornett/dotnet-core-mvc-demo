0fa283c6d4e52d04a5849ab1b9c37988809191f5
Ian Cornett <ian.cornett@slalom.com>
2020-01-08 12:55:53 -0800

    setup script with new pipeline

---
+++ infrastructure/New-WebApp.ps1
@@ -0,0 +1,17 @@
+﻿param
+(
+  [String]
+  [Parameter(Mandatory)]
+  $Name,
+
+  [String]
+  [Parameter(Mandatory)]
+  $ResourceGroupName,
+
+  [String]
+  $Location = 'westus2'
+)
+
+$rg = New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+$app = New-AzWebApp -ResourceGroupName $rg.ResourceGroupName -Name $Name -Location $Location
